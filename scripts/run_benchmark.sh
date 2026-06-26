72a654b84c9365317cb882a790c97c0da4e15fee
Felix Li <felix.li@amd.com>
2026-06-26 12:08:21 +0800

    layout api copyatom nt support (#745)

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -716,7 +716,7 @@ if [ "${RUN_PRESHUFFLE_GEMM}" -eq 1 ] && [ "${IS_CDNA}" = "true" ]; then
     dtype=$1; M=$2; N=$3; K=$4; tile_m=$5; tile_n=$6; tile_k=$7
     v2_flag=""
     case "$dtype" in
-      fp8 | fp16 | bf16) v2_flag="--use_v2" ;;
+      fp8 | int8 | fp16 | bf16) v2_flag="--use_v2" ;;
     esac
     log="${BENCH_LOG_DIR}/preshuffle_gemm_${M}x${N}x${K}_${dtype}_t${tile_m}x${tile_n}x${tile_k}.log"
     # shellcheck disable=SC2086 # v2_flag is intentionally unquoted (empty = omit)
@@ -754,6 +754,11 @@ if [ "${RUN_PRESHUFFLE_GEMM}" -eq 1 ] && [ "${IS_CDNA}" = "true" ]; then
     IFS=$oldIFS
     dtype=$1; M=$2; N=$3; K=$4; tile_m=$5; tile_n=$6; tile_k=$7
     shape_waves_per_eu="${8:-}"
+
+    v2_flag=""
+    case "$dtype" in
+      fp8 | int8 | fp16 | bf16) v2_flag="--use_v2" ;;
+    esac
 
     async_copy_flag=""
     async_copy_tag="async_copy"
